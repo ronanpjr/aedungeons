14e86996def5d01f04828d7002bcc3ed2a0158e3
Ronan Junior <ronanpereira@estudante.ufscar.br>
2023-11-16 14:55:05 -0300

    ///?????

---
+++ Assets/Scripts/HealthNode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthNode : MonoBehaviour
+{
+    public int health;
+    public bool healthType;
+    public HealthNode next;
+        
+    
+}
